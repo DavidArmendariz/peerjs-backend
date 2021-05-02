6a4c6a16931a2e03473f3d716d1ad9b09ddccaa4
David Armendariz <darmendariz1998@outlook.com>
2021-05-02 17:05:43 -0500

    Added peer server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-import server from 'http';
+import httpServer from 'http';
 import socketIO from 'socket.io';
 import { v4 as uuidV4 } from 'uuid';
+import { ExpressPeerServer } from 'peer';
 
 const app = express();
-const serve = server.Server(app);
-const io = socketIO(serve);
+const server = httpServer.Server(app);
+const io = socketIO(server);
+const peerServer = ExpressPeerServer(server);
 const port = process.env.PORT || 5000;
 
 // Middlewares
@@ -14,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.use('/peerjs', peerServer);
 app.get('/join', (req, res) => {
   res.send({ link: uuidV4() });
 });
@@ -21,29 +25,30 @@ app.get('/join', (req, res) => {
 io.on('connection', (socket) => {
   console.log('socket established');
   socket.on('join-room', (userData) => {
-    const { roomID, userID } = userData;
-    socket.join(roomID);
-    socket.to(roomID).broadcast.emit('new-user-connect', userData);
+    console.log('join-room event received', JSON.stringify(userData, null, 2));
+    const { roomId, userId } = userData;
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('new-user-connect', userData);
     socket.on('disconnect', () => {
-      socket.to(roomID).broadcast.emit('user-disconnected', userID);
+      socket.to(roomId).broadcast.emit('user-disconnected', userId);
     });
     socket.on('broadcast-message', (message) => {
-      socket.to(roomID).broadcast.emit('new-broadcast-messsage', { ...message, userData });
+      socket.to(roomId).broadcast.emit('new-broadcast-messsage', { ...message, userData });
     });
     // socket.on('reconnect-user', () => {
     //     socket.to(roomID).broadcast.emit('new-user-connect', userData);
     // });
     socket.on('display-media', (value) => {
-      socket.to(roomID).broadcast.emit('display-media', { userID, value });
+      socket.to(roomId).broadcast.emit('display-media', { userId: userId, value });
     });
     socket.on('user-video-off', (value) => {
-      socket.to(roomID).broadcast.emit('user-video-off', value);
+      socket.to(roomId).broadcast.emit('user-video-off', value);
     });
   });
 });
 
-// Server listen initilized
-serve
+// Server listen initialized
+server
   .listen(port, () => {
     console.log(`Listening on the port ${port}`);
   })
